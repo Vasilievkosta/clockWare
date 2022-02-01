b962ce775fd8ce066198f06d6ea20e3871c90de5
Vasilievkosta <vasilievkosta@gmail.com>
2022-02-01 18:39:21 +0200

    modified Form.js

--- src/App.js
+++ src/App.js
@@ -2,19 +2,11 @@ import React from 'react';
 
 import Form from './Form';
 
-const linkData = {
-  name: 'Ваше имя, не менее трех символов',
-  email: 'email',
-  size: 'Размер Ваших часов',
-  sity: 'Город',
-  time: 'Дата и время',
-};
-
 function App() {
 
   return (
     <>
-      <Form linkData={linkData} />
+      <Form />
     </>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -1,33 +1,98 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './form.css';
 
+class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            client: '',
+            email: '',
+            size: '',
+            sity: 'Днепр',
+            time: ''
+        };
+        this.onChangeHandler = this.onChangeHandler.bind(this);
+    }
 
-function Form(props) {
-    return (
-        <div className="field">
-            <h2 className="field__title">Cklock Ware</h2>
-            <p class="field__text">Для выбора мастера заполните пожалуйста данные</p>
+    onChangeHandler(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
 
-            <form class="field__form">
-                <label for="name" hidden>Имя</label>
-                <input class="field__input" id="name" type="text" placeholder={props.linkData.name} />
+    render() {
+        return (
+            <>
+                <div className="field">
+                    <h2 className="field__title">Cklock Ware</h2>
+                    <p className="field__text">Для выбора мастера заполните пожалуйста данные</p>
 
-                <label for="email" hidden>Email</label>
-                <input class="field__input" id="email" type="tel" placeholder={props.linkData.email} />
+                    <form className="field__form">
+                        <label for="client" hidden>Имя</label>
+                        <input className="field__input" id="client" name="client" type="text"
+                            placeholder="Ваше имя не менее трех букв" onChange={this.onChangeHandler} />
 
-                <label for="size" hidden>Размер часов</label>
-                <input class="field__input" id="size" type="tel" placeholder={props.linkData.size} />
+                        <label for="email" hidden>Email</label>
+                        <input className="field__input" id="email" name="email" type="tel"
+                            placeholder="Ваш e-mail" onChange={this.onChangeHandler} />
 
-                <label for="sity" hidden>Город</label>
-                <input class="field__input" id="sity" type="tel" placeholder={props.linkData.sity} />
+                        <div className="field__size">
+                            <label className="field__radio">Размер часов: </label>
+                            <label className="field__radio">Маленькие
+                                <input id="size" name="size" type="radio" value="small"
+                                    checked={this.state.size === "small"}
+                                    onChange={this.onChangeHandler}
+                                />
+                            </label>
 
-                <label for="time" hidden>Дата</label>
-                <input class="field__input" id="size" type="tel" placeholder={props.linkData.time} />
-            </form>
+                            <label className="field__radio">Средние
+                                <input id="size" name="size" type="radio" value="medium"
+                                    checked={this.state.size === "medium"}
+                                    onChange={this.onChangeHandler}
+                                />
+                            </label>
 
-            <a class="field__link" href="#">Выбор мастера</a>
-        </div>
-    )
-}
+                            <label className="field__radio">Большие
+                                <input id="size" name="size" type="radio" value="big"
+                                    checked={this.state.size === "big"}
+                                    onChange={this.onChangeHandler}
+                                />
+                            </label>
+                        </div>
+
+
+
+
+                        <label for="sity" hidden>Город</label>
+                        <select className="field__input" id="sity" name="sity" type="text" value={this.state.sity}
+                            onChange={this.onChangeHandler}>
+                            <option className="field__sity" value="Днепр">Днепр</option>
+                            <option className="field__sity" value="Ужгород">Ужгород</option>
+                            <option className="field__sity" value="Киев">Киев</option>
+                            <option className="field__sity" value="Одесса">Одесса</option>
+                        </select>
+
+                        <label for="time" hidden>Дата</label>
+                        <input className="field__input" id="size" name="time" type="tel"
+                            placeholder="Введите дату" onChange={this.onChangeHandler} />
+                    </form>
+
+                    <a className="field__link" href="#">Выбор мастера</a>
+                </div>
+                <br />
+                <hr />
+                <div className="state">
+                    <p>State of Component</p>
+                    <pre>{JSON.stringify(this.state, null, 2)}</pre>
+                </div>
+
+
+
+            </>
+
+        );
+    }
+};
+
 
 export default Form;
